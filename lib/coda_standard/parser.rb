71a515ec170782b8640a50a550ea091b77d41c6b
Al <pursuance@gmail.com>
2015-04-02 14:38:50 +0200

    parsed entry date, ref number, account

--- lib/coda_standard/parser.rb
+++ lib/coda_standard/parser.rb
@@ -3,21 +3,29 @@ module CodaStandard
 
     def initialize
       @transactions = []
+      @old_balance = ""
     end
 
     def parse(file_name)
       File.open( file_name ).each do |line|
+        if line =~ /^1/
+          set_old_balance(line)
+        end
         if line =~ /^21/
           create_transaction
+          set_entry_date(line)
           set_amount(line)
+          set_reference_number(line)
         end
         if line =~ /^23/
           set_name(line)
+          set_account(line)
         end
         if line =~ /^32/
           set_address(line)
         end
       end
+      @transactions.each{|transaction| transaction.old_balance = @old_balance}
       @transactions
     end
 
@@ -31,22 +39,47 @@ module CodaStandard
       name = line.scan(regex_name).join.strip.gsub!(regex_currencies, "")
       @transactions.last.name = name if name != ""
     end
+    def set_entry_date(line)
+      regex_entry_date = /^21.{113}(\d{6})/
+      entry_date = line.scan(regex_entry_date).join.strip
+      @transactions.last.entry_date = entry_date if entry_date != ""
+    end
+    def set_reference_number(line)
+      regex_reference_number = /^21.{8}(.{21})/
+      reference_number = line.scan(regex_reference_number).join.strip
+      @transactions.last.reference_number = reference_number if reference_number != ""
+    end
     def set_address(line)
       regex_address = /^32\d{8}(.+)[10]\s[10]$/
       address = line.scan(regex_address).join.strip
       @transactions.last.address = address if address != ""
     end
+    def set_account(line)
+      regex_account = /^23\d{8}(\w+)\s/
+      account = line.scan(regex_account).join.strip
+      @transactions.last.account = account if account != ""
+    end
     def set_amount(line)
-
-      regex_amount = /^21\d+\s+\d(\d{15})/
-      regex_clean_zeros = /0+([^0]\d+)(\d{3})/
-      # previous number involving credit or debit skipped
+      regex_amount = /^21\d+\s+(\d{16})/
+      regex_clean_zeros = /0*([^0]\d+)(\d{3})/
       amount = line.scan(regex_amount).join
+      amount[0] == "0" ? amount_sign = "" : amount_sign = "-"
       amount_integral = amount.scan(regex_clean_zeros)[0][0]
       amount_decimals = amount.scan(regex_clean_zeros)[0][1]
       separator = ","
-      amount = amount_integral + separator + amount_decimals
+      amount = amount_sign + amount_integral + separator + amount_decimals
       @transactions.last.amount = amount if amount != ""
+    end
+    def set_old_balance(line)
+      regex_amount = /^1.{41}(\d)(\d{15})/
+      regex_clean_zeros = /0*([^0]\d+)(\d{3})/
+      amount = line.scan(regex_amount).join
+      amount[0] == "0" ? amount_sign = "" : amount_sign = "-"
+      amount_integral = amount.scan(regex_clean_zeros)[0][0]
+      amount_decimals = amount.scan(regex_clean_zeros)[0][1]
+      separator = ","
+      amount = amount_sign + amount_integral + separator + amount_decimals
+      @old_balance = amount if amount != ""
     end
   end
 end
--- lib/coda_standard/transaction.rb
+++ lib/coda_standard/transaction.rb
@@ -3,5 +3,9 @@ module CodaStandard
     attr_accessor :name
     attr_accessor :address
     attr_accessor :amount
+    attr_accessor :old_balance
+    attr_accessor :account
+    attr_accessor :entry_date
+    attr_accessor :reference_number
   end
 end
