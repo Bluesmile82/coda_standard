fdaa48b38ad5539582745b6fa23531508c3dab65
Romain Gérard <romaingweb@gmail.com>
2015-04-15 10:02:02 +0200

    Use let blocks instead of instance variable in specs

--- spec/transaction_list_spec.rb
+++ spec/transaction_list_spec.rb
@@ -1,20 +1,18 @@
 require_relative 'spec_helper'
 
 describe CodaStandard::TransactionList do
-  before :each do
-    @transaction_list = CodaStandard::TransactionList.new
-  end
+  let(:transaction_list) { CodaStandard::TransactionList.new }
 
   describe "initialize some values" do
     it "has an transactions array" do
-      expect(@transaction_list.transactions).to eq([])
+      expect(transaction_list.transactions).to eq([])
     end
   end
 
   describe "create" do
     it "creates a new transaction and puts it into the array" do
-      @transaction_list.create
-      expect(@transaction_list.create).to eq @transaction_list.transactions.last
+      transaction_list.create
+      expect(transaction_list.create).to eq transaction_list.transactions.last
     end
   end
 end
